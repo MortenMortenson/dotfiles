a9feb96a78471589b8e746a149a03036aad9c6c1
Morten <kugalskaper@gmail.com>
2021-12-30 19:56:11 +0100

    changes

--- installnotes.sh
+++ installnotes.sh
@@ -2,19 +2,34 @@
  alsa-utils alsa mixer pulseaudio pulseaudio-bluetooth pulsemixer
 
 #Bluetooth
-bluetoothctl bluetooth-utils
+bluez bluez-utils  -- sudo systemctl enable bluetooth -- bluetoothctl adapter on, power on, scan on, pair-connect-trust. /etc/bluetooth/main.conf AutoEnable=true FastConnectable = true
+
+#Editor
+vim nvim
 
 #Fonts
 nerd-fonts-hack
+nerd-fonts-monokai
 
 #Terminal
-Alacritty theme.sh zsh fzf 
+Alacritty theme.sh zsh fzf zsh4human exa 
 
 #Backup
 timeshift
 
 #wm
-i3-gaps i3-resurrect polybar
+i3-gaps i3-resurrect polybar unclutter
 
-#Music 
-spotifyd spotify-tui
+#Media 
+spotifyd spotify-tui mpv cava
+
+#graphics
+nvidia-settings -- xserver display configuration - advanced - force full composition pipeline
+#Filemanager
+ranger
+
+#browser
+paru -S google-chrome
+
+#utilities
+stow bpytop flameshot feh
