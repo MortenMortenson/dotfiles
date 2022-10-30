e7a3a567576f815f1e3f3e165a7249d4a0649a2e
MortenMortenson <kugalskaper@gmail.com>
2022-10-30 22:21:57 +0100

    Delete .scipts directory

--- .scipts/install.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-#update and install packages 
-sudo pacman -Syu
-sudo pacman -S neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxkhd dunst rofi polybar pulsemixer git man zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
-
-# clone repo
-sudo git clone 
