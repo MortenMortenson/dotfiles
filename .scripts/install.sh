e81bdc0fb6260c43dcd8c7b6e61f81c108cfd4eb
MortenMortenson <kugalskaper@gmail.com>
2022-10-30 22:49:08 +0100

    Update install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -2,7 +2,7 @@
 
 #update and install packages 
 sudo pacman -Syu
-sudo pacman -S --needed base-devel pulseaudio pulseaudio-bluetooth alsamixer snapper alsa-utils neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxhkd dunst rofi polybar pulsemixer git zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
+sudo pacman -S --needed base-devel pulseaudio pulseaudio-bluetooth snapper alsa-utils neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxhkd dunst rofi polybar pulsemixer git zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
 
 
 #systemd
