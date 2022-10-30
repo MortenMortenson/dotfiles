1a3186e35f826d49762f37b0c1e1b5f12a333f38
MortenMortenson <kugalskaper@gmail.com>
2022-10-30 22:12:01 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+#update and install packages 
+sudo pacman -Syu
+sudo pacman -S neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxkhd dunst rofi polybar pulsemixer git man zsh nitrogen arandr 
+
+# clone repo
+sudo git clone 
