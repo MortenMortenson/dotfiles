e18dcd0a600693bbd4a85d053a4008aa2c3ca808
MortenMortenson <kugalskaper@gmail.com>
2022-10-30 22:13:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #update and install packages 
 sudo pacman -Syu
-sudo pacman -S neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxkhd dunst rofi polybar pulsemixer git man zsh nitrogen arandr 
+sudo pacman -S neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxkhd dunst rofi polybar pulsemixer git man zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
 
 # clone repo
 sudo git clone 
