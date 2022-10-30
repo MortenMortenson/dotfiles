6c340e86f10114ee667cda4311b43c06a3bb3946
MortenMortenson <kugalskaper@gmail.com>
2022-10-30 22:45:49 +0100

    Update install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -2,7 +2,7 @@
 
 #update and install packages 
 sudo pacman -Syu
-sudo pacman -S --needed base-devel pulseaudio pulseaudio-bluetooth alsa-mixer snapper alsa-utils neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxhkd dunst rofi polybar pulsemixer git man zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
+sudo pacman -S --needed base-devel pulseaudio pulseaudio-bluetooth alsa-mixer snapper alsa-utils neovim unclutter exa bat ranger alacritty firefox feh ueberzug sxhkd dunst rofi polybar pulsemixer git zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
 
 
 #systemd
@@ -10,5 +10,5 @@ sudo systemctl enable bluetooth
 
 # stow
 rm -rf ~/.config/i3
-cd dotfiles
+cd ~/dotfiles
 stow -v
