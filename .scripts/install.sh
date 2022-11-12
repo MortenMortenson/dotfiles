473de3ec33f8fc97d500b263bd0faaafba2c9c6c
MortenMortenson <kugalskaper@gmail.com>
2022-11-12 20:57:12 +0000

    Update install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -2,7 +2,7 @@
 
 #update and install packages 
 sudo pacman -Syu
-sudo pacman -S --needed --noconfirm base-devel pulseaudio pulseaudio-bluetooth snapper alsa-utils picom neovim unclutter exa bat ranger pcmanfm alacritty firefox feh ueberzug sxhkd dunst rofi polybar pulsemixer git zsh nitrogen arandr stow bpytop bluez bluez-utils cups 
+sudo pacman -S --needed --noconfirm base-devel pulseaudio pulseaudio-bluetooth snapper alsa-utils picom neovim unclutter exa bat ranger pcmanfm alacritty firefox feh ueberzug sxhkd dunst rofi polybar pulsemixer git zsh nitrogen arandr stow bpytop bluez bluez-utils cups python-pywal thunar 
 
 #install yay
 #cd /opt
@@ -39,7 +39,7 @@ git clone https://github.com/Aloxaf/fzf-tab
 
 #Bluetoothsettings
 #sed -i "#FastConnectable = false/FastConnectable = true/g" /etc/bluetooth/main.conf
-#sed -i "#AutoEnable=false/AutoEnable=true/g" /etch/bluetooth/main.conf
+#sed -i "#AutoEnable=false/AutoEnable=true/g" /etc/bluetooth/main.conf
 
 #set caps to escape
-sed '/Option "XkbLayout" "no"/a \ \ \ \ \ \ \  Option "XkbOptions" "caps:escape"' 00-keyboard.conf.bak
+sed -i '/Option "XkbLayout" "no"/a \ \ \ \ \ \ \  Option "XkbOptions" "caps:escape"' 00-keyboard.conf.bak
