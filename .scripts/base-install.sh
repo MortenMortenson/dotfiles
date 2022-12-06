2c26aeac9c70bc3fe2dd5ee960bbc70a5bb612f5
MortenMortenson <kugalskaper@gmail.com>
2022-12-06 22:07:54 +0100

    Update base-install.sh

--- .scripts/base-install.sh
+++ .scripts/base-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-#Setup btrfs subvolumes and mount
+#######Setup btrfs subvolumes and mount
 #Ask for name of disk
 echo Whats the name of the root disk
 read rootdisk
 echo Whats the name of the boot disk
 read bootdisk
+echo What is your root password
+read rootpassword
 
 mount /dev/$rootdisk /mnt
 cd /mnt
@@ -26,5 +28,35 @@ mount -o compress=zstd:1,noatime,subvol=@log /dev/$rootdisk /mnt/var/log
 mount -o compress=zstd:1,noatime,subvol=@images /dev/$rootdisk /mnt/var/lib/libvirt/images
 mount /dev/$bootdisk /mnt/boot
 pacman -S --noconfirm archlinux-keyring
-pacstrap /mnt base vim reflector git
+pacstrap /mnt base vim reflector git zsh
 genfstab -U /mnt >> /mnt/etc/fstab
+
+###Post install
+arch-chroot /mnt
+#locale and time
+ln -sf /usr/share/zoneinfo/Europe/Oslo /etc/localtime
+hwclock --systohc
+sed 's/#en_US.UTF-8/en_US.UTF8/g' /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+echo "KEYMAP=no-latin1" >> /etc/vconsole.conf
+
+#Mirrors and pacman
+reflector --country NO --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+sed 's/#ParallelDownloads/ParallelDownloads/g' /etc/pacman.conf
+pacman -Syy
+
+#Hostname
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+
+#Users and password
+echo root:$rootpassword | chpasswd
+useradd -m -G wheel -s /bin/zsh morten
+echo morten:$rootpassword | chpasswd
+
+#Exports
+#export VISUAL=nano
+
